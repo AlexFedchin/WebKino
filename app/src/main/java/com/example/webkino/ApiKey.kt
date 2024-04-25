5c738d079bb33a230e2b13739089462ba6f6d27a
Alexander <alexfedchin22@gmail.com>
2024-04-25 13:42:16 +0300

    Fixed .gitignore, added new .apk file

--- app/src/main/java/com/example/webkino/ApiKey.kt
+++ app/src/main/java/com/example/webkino/ApiKey.kt
@@ -1,4 +1,4 @@
 package com.example.webkino
 
-// Define API key
+// Define API key (not to be used in own purposes, provided to the teacher only in testing purposes)
 const val API_KEY = "abf3a6ac8c9d2e33c91d318ea187ea19"
